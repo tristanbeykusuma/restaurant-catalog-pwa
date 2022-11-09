38696bb4f5e07ba21762e65cb8422d20a13808f2
tristanbeykusuma <tristanbeykusuma@gmail.com>
2022-11-09 15:54:01 +0800

    add hero responsive

--- src/scripts/components/hero.js
+++ src/scripts/components/hero.js
@@ -7,9 +7,9 @@ class Hero extends HTMLElement {
     this.innerHTML = `
           <div class="hero">
               <picture>
-                <source class="img-responsive" media="(max-width: 600px)" srcset="./images/heros/hero-image_2-small.jpg">
-                <source class="img-responsive" media="(min-width: 601px)" srcset="./images/heros/hero-image_2-extra.jpg">
-                <img class="img-responsive" src='./images/heros/hero-image_2-extra.jpg' 
+                <source media="all and (max-width: 600px)" srcset="./images/heros/hero-image_2-small.jpg">
+                <source media="all and (min-width: 601px)" srcset="./images/heros/hero-image_2-extra.jpg">
+                <img src='./images/heros/hero-image_2-extra.jpg' 
                   alt="discover foods hero image">
               </picture>
               <h2 class="hero__title">Discover Foods</h2>
